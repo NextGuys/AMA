140ba69b6d6161f2c13db640b2006bb8e9619144
折田幸夫 <yukio@oritayukionoMacBook-Pro.local>
2019-09-20 03:34:09 +0900

    add home and profile

---
+++ client/pages/profile.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../components/Header'
+import { Container } from '../components/Container'
+import styled from 'styled-components'
+import Link from 'next/link'
+
+export default () => (
+  <>
+    <Header>Profile</Header>
+
+    <Container />
+  </>
+)
