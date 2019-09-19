81655daca550a9872953a4a54f5e37ecd685cae3
Shogo Yasuda <shgysd@gmail.com>
2019-09-19 17:22:42 +0900

    Setup api

---
+++ server/api/interfaces/user.go
@@ -0,0 +1,73 @@
+package interfaces
+
+import (
+	"net/http"
+
+	repo "github.com/NextGuys/AMA/server/api/repository"
+
+	"github.com/NextGuys/AMA/server/api/model"
+	"github.com/NextGuys/AMA/server/api/utils"
+	"github.com/jinzhu/gorm"
+	"github.com/labstack/echo"
+	"github.com/rs/xid"
+)
+
+// NewUserRepo Initialize user repository
+func NewUserRepo(conn *gorm.DB) repo.UserRepo {
+	return &UserRepo{
+		Conn: conn,
+	}
+}
+
+// UserRepo Handler with DB
+type UserRepo struct {
+	Conn *gorm.DB
+}
+
+// SignUp SignUp
+func (h *UserRepo) SignUp(u *model.User) error {
+
+	// Validate
+	if u.Email == "" || u.Password == "" {
+		return echo.NewHTTPError(http.StatusUnauthorized, "Please provide valid cred")
+	}
+
+	pwd := []byte(u.Password)
+	hash := utils.HashAndSalt(pwd)
+	uid := xid.New()
+
+	u.UID = uid
+	u.Password = hash
+	if !h.Conn.NewRecord(&u) {
+		panic("could not create new record")
+	}
+	if err := h.Conn.Create(&u).Error; err != nil {
+		panic(err.Error())
+	}
+
+	return nil
+}
+
+// Login Login
+func (h *UserRepo) Login(u *model.User) error {
+
+	// Validate
+	if u.Email == "" || u.Password == "" {
+		return echo.NewHTTPError(http.StatusUnauthorized, "Please provide valid cred")
+	}
+
+	pwd := []byte(u.Password)
+	hash := utils.HashAndSalt(pwd)
+	uid := xid.New()
+
+	u.UID = uid
+	u.Password = hash
+	if !h.Conn.NewRecord(&u) {
+		panic("could not create new record")
+	}
+	if err := h.Conn.Create(&u).Error; err != nil {
+		panic(err.Error())
+	}
+
+	return nil
+}
---
+++ server/api/model/message.go
@@ -0,0 +1,17 @@
+package model
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+// Message UserModel
+type Message struct {
+	gorm.Model
+	UserID  int    `json:"user_id"`
+	RoomID  int    `json:"room_id"`
+	Content string `json:"content"`
+	Rooms   []Room `gorm:"many2many:room_users;"`
+}
+
+// Messages List users
+var Messages []Message
