4dacdb7e34e3fe0d50ff3c42095cf9635bec15fa
折田幸夫 <yukio@oritayukionoMacBook-Pro.local>
2019-09-19 20:21:34 +0900

    add components

---
+++ client/components/MyLayout.js
@@ -0,0 +1,30 @@
+// import Header from './Header'
+// import styled from 'styled-components'
+
+// const layoutStyle = {
+//   margin: 20,
+//   padding: 20,
+//   border: '1px solid #DDD'
+// }
+
+// const Header = styled.header`
+//   font-size: 1.5rem;
+//   padding: 2rem;
+//   text-align: center;
+//   color: #ffc507;
+//   background: #353b41;
+// `
+
+// export default Header
+
+// const Layout = props => (
+//   console.log(props),
+//   (
+//     <div style={layoutStyle}>
+//       <Header />
+//       {props.children}
+//     </div>
+//   )
+// )
+
+// export default Layout
---
+++ client/components/molecules/FormField/index.js
@@ -0,0 +1,48 @@
+// import React from 'react'
+// import styled from 'styled-components'
+// // import styles from './styles.scss'
+// // // import Label from 'shared/components/atoms/Label'
+// // // import cssModuleNameTag from '../../cssModuleNameTag'
+// // // const cssModules = new cssModuleNameTag(styles)
+
+// const FormStyle = styled.div`
+//   /* BookList固有のWrapperスタイル */
+// `
+
+// export default function FormField (props) {
+//   const {
+//     className = '',
+//     children: [leftChild, rightChild],
+//     labelPosition,
+//     labelWidth,
+//     style,
+//     labelStyle,
+//     inputStyle,
+//     ...rest
+//   } = props
+
+//   return (
+//     <div
+//       // className={cssModules`root ${className}`}
+//       style={style}
+//       data-label-width={labelWidth}
+//       {...rest}
+//     >
+//       <div
+//         // className={cssModules`left`}
+//         data-position={labelPosition}
+//         style={labelStyle}
+//         data-label-width={labelWidth}
+//       >
+//         {leftChild}
+//       </div>
+//       <div
+//         // className={cssModules`right`}
+//         style={inputStyle}
+//         data-label-width={labelWidth}
+//       >
+//         {rightChild}
+//       </div>
+//     </div>
+//   )
+// }
